b84980f4c180c64df555f2a6905ea1973c2a08c2
Krzysztof Mazurkiewicz <k.mazurkiewicz.91@gmail.com>
2018-05-27 16:09:17 +0200

    adding, editing and deleting categories

--- todolist/tasks/views.py
+++ todolist/tasks/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Category
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+def categories(request):
+    categories = Category.objects.all()
+    return render(request, 'categories.html', {
+        'categories': categories
+    })
+
+
+def add_category(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        color = request.POST.get('color')
+        if color and name:
+            category = Category(
+                name=name,
+                color=color
+            )
+            category.save()
+    return render(request, 'add_category.html')
+
+
+def edit_category(request, category_id):
+    category = Category.objects.get(id=category_id)
+    if request.method == "POST":
+        name = request.POST.get('name')
+        color = request.POST.get('color')
+        if color and name:
+            category.name=name
+            category.color=color
+            category.save()
+    return render(request, 'edit_category.html', {'category':category})
+
+
+def delete_category(request, category_id):
+    category = Category.objects.get(id=category_id)
+    category.delete()
+    return redirect('categories-list')
+
+
