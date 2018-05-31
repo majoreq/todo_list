872eee04400eae2d100b2c95c59cfa67a6824053
Krzysztof Mazurkiewicz <k.mazurkiewicz.91@gmail.com>
2018-05-31 13:36:49 +0200

    adding, editing, deleting categories and change views for class based

--- todolist/tasks/views.py
+++ todolist/tasks/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Category
+from .models import Category, Task
+from django.views import View
 
-def index(request):
-    return render(request, 'index.html')
-
-
-def categories(request):
-    categories = Category.objects.all()
-    return render(request, 'categories.html', {
-        'categories': categories
-    })
+class index(View):
+    def get(self, request):
+        return render(request, 'index.html')
 
 
-def add_category(request):
-    if request.method == "POST":
+class categories(View):
+    def get(self, request):
+        categories = Category.objects.all()
+        return render(request, 'categories.html', {
+            'categories': categories
+        })
+
+
+
+class add_category(View):
+    def post(self,request):
         name = request.POST.get('name')
         color = request.POST.get('color')
         if color and name:
@@ -23,24 +27,94 @@ def add_category(request):
                 color=color
             )
             category.save()
-    return render(request, 'add_category.html')
+            return redirect('categories-list')
 
 
-def edit_category(request, category_id):
-    category = Category.objects.get(id=category_id)
-    if request.method == "POST":
+    def get(self,request):
+        return render(request, 'add_category.html')
+
+
+class edit_category(View):
+    def post(self,request, category_id):
+        category = Category.objects.get(id=category_id)
         name = request.POST.get('name')
         color = request.POST.get('color')
         if color and name:
             category.name=name
             category.color=color
             category.save()
-    return render(request, 'edit_category.html', {'category':category})
+        return redirect('categories-list')
+    def get(self,request, category_id):
+        category = Category.objects.get(id=category_id)
+        return render(request, 'edit_category.html', {
+            'category':category
+        })
 
 
-def delete_category(request, category_id):
-    category = Category.objects.get(id=category_id)
-    category.delete()
-    return redirect('categories-list')
+class delete_category(View):
+    def get(self, request, category_id):
+        category = Category.objects.get(id=category_id)
+        category.delete()
+        return redirect('categories-list')
 
 
+class tasks(View):
+    def get(self, request):
+        tasks = Task.objects.all()
+        categories = Category.objects.all()
+        return render(request, 'tasks.html', {
+            'tasks':tasks,
+            'categories':categories
+        })
+
+
+class add_task(View):
+    def get(self, request):
+        categories = Category.objects.all()
+        return render(request, 'add_task.html', {
+            'categories':categories
+        })
+
+
+    def post(self, request):
+        category = request.POST.get('category')
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        task = Task()
+        task.category_id = category
+        task.name = name
+        task.description = description
+        task.save()
+        print(task.id)
+        print(task.name)
+        print(task.description)
+        print(task.category)
+        return redirect('tasks')
+
+
+class edit_task(View):
+    def get(self, request, task_id):
+        task = Task.objects.get(id=task_id)
+        categories = Category.objects.all()
+        return render(request, 'edit_task.html', {
+            'task':task,
+            'categories':categories
+        })
+
+    def post(self, request, task_id):
+        task = Task.objects.get(id=task_id)
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        category_id = request.POST.get('category')
+        if name and category_id and description:
+            task.name = name
+            task.description = description
+            task.category_id = category_id
+            task.save()
+        return redirect('tasks')
+
+class delete_task(View):
+    def get(self, request, task_id):
+        task = Task.objects.get(id=task_id)
+        task.delete()
+        return redirect('tasks')
--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 from django.urls import path
-from tasks.views import index, categories, add_category, edit_category, delete_category
+from tasks.views import index, categories, add_category, edit_category, delete_category, add_task, tasks, edit_task, delete_task
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('categories/', categories, name='categories-list'),
-    path('add_category/', add_category, name='add-category'),
-    path('edit_category/<int:category_id>', edit_category, name='edit-category'),
-    path('delete_category/<int:category_id>', delete_category, name='delete-category'),
+    path('', index.as_view(), name='index'),
+    path('categories/', categories.as_view(), name='categories-list'),
+    path('add_category/', add_category.as_view(), name='add-category'),
+    path('edit_category/<int:category_id>', edit_category.as_view(), name='edit-category'),
+    path('delete_category/<int:category_id>', delete_category.as_view(), name='delete-category'),
+    path('tasks/', tasks.as_view(), name='tasks'),
+    path('add_task/', add_task.as_view(), name='add-task'),
+    path('edit_task/<int:task_id>', edit_task.as_view(), name='edit-task'),
+    path('delete_task/<int:task_id>', delete_task.as_view(), name='delete-task'),
+
 ]
 
