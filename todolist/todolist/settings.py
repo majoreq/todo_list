46271a5f0822972e8fb26ed8f310304b9178a909
Krzysztof Mazurkiewicz <k.mazurkiewicz.91@gmail.com>
2018-09-15 10:42:43 +0200

    minor changes

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -125,8 +125,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-try:
-    from .localsettings import *
-except ImportError as e:
-    print("Configure your localsettings!!!")
